6036b9a0da619ed442b7b41edaa050f383b370a4
Itshat36 <112979025+Itshat36@users.noreply.github.com>
2023-01-23 14:37:36 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("Hello,World")
+# this is a single line commen
+
+""" Arithmetic Operators
++ add
+- subtract
+* multiply
+/ Divide
+// Floor divide (divide and round down to nearest int)
+% Mod (modulus/Remainder from Int divition)
+** Power
+"""
+
+"""Assiment Operators
+= (value Assiment) (X = y)
++= Add to (x += y is shorthand for X = X + y)
+-= subtract from (x -= y is shorthand for X = X - y)
+*=
+/=
+//+
+%=
+**=
+"""
+
+""" data type 
+int - integer (whole number)
+Float - FLoating point number (num w/ a decimal)
+str - String (any text, in quotes)
+bool - boolean (either True or false)
+chr - character (single keyboard charecter, in singel quotes)
+"""
---
+++ prog52a.py
@@ -0,0 +1,8 @@
+# Lang52a - area and Perimeter of a Rectangle
+length = int(input("Enter length:"))
+Width = int(input("Enter Width:"))
+area = length * Width
+perim = 2 * length + 2 * Width
+
+print ("Area =", area)
+print("perimeter = " + str(perim))
