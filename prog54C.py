c07b65f4502d0badc22bb71948e95dfa00a5e1a5
Itshat36 <112979025+Itshat36@users.noreply.github.com>
2023-01-23 14:46:50 +0000

    update

---
+++ prog54C.py
@@ -0,0 +1,4 @@
+a = int(input("Enter circuferance:"))
+
+s = a / 3.14159 * 2
+print("Sum=", s)
---
+++ prog54b.py
@@ -0,0 +1,9 @@
+a = int(input("Enter A:"))
+b = int(input("Enter B:"))
+c = int(input("Enter C:"))
+d = int(input("Enter D:"))
+
+s = a + b + c + d
+ave = s / 4
+print("Sum=", s)
+print("ave=", ave)
